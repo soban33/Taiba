4b11f2eacc8aeb71046d6303109a98f4ef267e6f
soban33 <sobanp33@gmail.com>
2020-07-24 21:02:30 +0530

    hello world

---
+++ app/src/main/java/com/example/taiba/ui/slideshow/SlideshowFragment.kt
@@ -0,0 +1,25 @@
+package com.example.taiba.ui.slideshow
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.taiba.R
+
+class SlideshowFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
+        return root
+    }
+}
